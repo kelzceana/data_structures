bb732f7f99d3c2932fcacd88094f5e3093754775
Kelechi Obikaonu <kelechipauline@gmail.com>
2021-03-21 17:03:19 -0400

    algorithim implementation with stack

---
+++ stack.js
@@ -0,0 +1,80 @@
+// functions implemented using stack
+
+const isPalindrom = (str) => {
+  const stack = []
+  let rStr = ""
+
+  for ( i = 0; i < str.length; i++) {
+    stack.push(str[i])
+  }
+  
+  for (i = 0; i < str.length; i++) {
+    rStr += stack.pop()
+  }
+  
+  if (rStr === str) return true;
+
+  return false
+}
+
+//console.log(isPalindrom("bob"))
+
+
+var buildArray = function(target, n) {
+  const newStack = []
+  let i = 0
+  
+  while (i++ <= target.length && n) {
+    if (i !== target[0]) {
+      newStack.push("push", "pop")
+      
+    } else {
+      newStack.push("push")
+      target = target.slice(1)
+    }
+  }
+  
+  return newStack
+};
+
+var buildArray = function(target, n) {
+	let res = []
+	let i = 0
+
+	while (i++ <= n && target.length) {
+		if (i !== target[0]) res.push("Push", "Pop")
+		else {
+			res.push("Push")
+			target = target.slice(1)
+		}
+	}
+	return res
+}
+
+
+console.log(buildArray([1,3], 3))
+
+//3
+/**
+ * 
+ * Input: students = [1,1,0,0], sandwiches = [0,1,0,1]
+   Output: 0 
+ */
+
+const countStudents = function(students, sandwiches) {
+  
+  while (students.length) {
+    const topSand = sandwiches[0];
+
+    if (!students.includes(topSand)) {
+      return students.length;
+    }
+
+    if (firstStudent === sandwiches[0]){
+      students.shift()
+      sandwiches.shif()
+    } else {
+      students.push(students.shift())
+    }
+  }
+};
